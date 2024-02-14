f368ccbc59ddccf2047d10c2315c5c11b413f55c
Hemalathas25 <hemalathasm04@gmail.com>
2024-02-14 12:19:13 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,66 @@
+{/* Operators
+Assignment operators
+=  +=  -=  *=  /=  %=  **=
+
+Arithmetic Operators
+Arithmetic operators are mathematical operators.
+
+Addition(+): a + b
+Subtraction(-): a - b
+Multiplication(*): a * b
+Division(/): a / b
+Modulus(%): a % b
+Exponential(**): a ** b
+
+Comparison Operators:   
+ ==  Equal in value only equivalent
+ === equal in value and data type exactly equal
+ != not equal
+ !== not equal value or not equal type
+ < 
+ <= 
+ > 
+ >= 
+
+ logical Operators:
+ && AND 
+ 1 1 1
+ 1 0 0
+ 0 1 0 
+ 0 0 0
+
+ || OR
+ 1 1 1
+ 1 0 1 
+ 0 1 1
+ 0 0 0
+
+ ! NOT
+ 1 0
+ 0 1
+
+Increment Operator
+    In JavaScript we use the increment operator to increase a value stored in a variable.
+The increment could be pre or post increment
+ => Pre-increment
+let count = 0
+console.log(++count)        // 1
+console.log(count)          // 1
+
+=> Post-increment
+let count = 0
+console.log(count++)        // 0
+console.log(count)          // 1
+
+Decrement Operator
+
+=> Pre-decrement
+let count = 0
+console.log(--count) // -1
+console.log(count)  // -1
+=> Post-decrement
+let count = 0
+console.log(count--) // 0
+console.log(count)   // -1
+
+*/}
