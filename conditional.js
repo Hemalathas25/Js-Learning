48bca1ed0bb76cf8966cd0ad06d376dd0d439cbe
Hemalathas25 <hemalathasm04@gmail.com>
2024-02-14 17:08:35 +0530

    conditional

---
+++ conditional.js
@@ -0,0 +1,12 @@
+{/*Conditionals
+Conditional statements are used for make decisions based on different conditions.
+ By default , statements in JavaScript script executed sequentially from top to bottom.
+  If the processing logic require so, the sequential flow of execution can be altered in two ways:
+
+Conditional execution: a block of one or more statements will be executed if a certain 
+expression is true
+Repetitive execution: a block of one or more statements will be repetitively executed as 
+long as a certain expression is true. In this section, we will cover if, else ,
+else if statements.
+
+*/}
