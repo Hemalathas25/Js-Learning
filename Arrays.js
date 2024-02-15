50127b0e501e5c5f8907c33cf1038e20e742e8b6
Hemalathas25 <hemalathasm04@gmail.com>
2024-02-15 12:53:41 +0530

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,348 @@
+/*Arrays
+In contrast to variables, an array can store multiple values. 
+Each value in an array has an index, and each index has a reference 
+in a memory address. Each value can be accessed by using their indexes. 
+The index of an array starts from zero, and the index of the last element 
+is less by one from the length of the array.
+
+An array is a collection of different data types which are
+ ordered and changeable(modifiable). An array allows storing duplicate
+  elements and different data types. An array can be empty,
+   or it may have different data type values. 
+
+   How to create an empty array
+
+ Using Array constructor
+// syntax
+const arr = Array()
+// or
+// let arr = new Array()
+console.log(arr) // []
+
+Using square brackets([])
+// syntax
+// This the most recommended way to create an empty list
+const arr = []
+console.log(arr)
+*/
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
+
+/*Numbers: [0, 3.14, 9.81, 37, 98.6, 100]
+Number of numbers: 6
+Fruits: ['banana', 'orange', 'mango', 'lemon']
+Number of fruits: 4
+Vegetables: ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+Number of vegetables: 5
+Animal products: ['milk', 'meat', 'butter', 'yoghurt']
+Number of animal products: 4
+Web technologies: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB']
+Number of web technologies: 7
+Countries: ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+Number of countries: 5 */
+
+//Creating an array using split
+   
+    let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+// the text has special characters think how you can just get only the words
+// ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+
+// Accessing array items using index
+
+const fruitss = ['banana', 'orange', 'mango', 'lemon']
+let firstFruit = fruitss[0] // we are accessing the first item using its index
+
+console.log(firstFruit) // banana
+
+secondFruit = fruitss[1]
+console.log(secondFruit) // orange
+
+let lastFruit = fruitss[3]
+console.log(lastFruit) // lemon
+// Last index can be calculated as follows
+
+let lastIndex = fruitss.length - 1
+lastFruit = fruitss[lastIndex]
+
+console.log(lastFruit)  // lemon
+
+// Another
+
+const numberss = [0, 3.14, 9.81, 37, 98.6, 100]  // set of numbers
+
+console.log(numberss.length)  // => to know the size of the array, which is 6
+console.log(numberss)         // -> [0, 3.14, 9.81, 37, 98.6, 100]
+console.log(numberss[0])      //  -> 0
+console.log(numberss[5])      //  -> 100
+
+let lastIndexx = numberss.length - 1;
+console.log(numberss[lastIndexx]) // -> 100
+
+// Modifying array element
+
+//An array is mutable(modifiable). Once an array is created, 
+//we can modify the contents of the array elements.
+
+const number = [1, 2, 3, 4, 5]
+number[0] = 10      // changing 1 at index 0 to 10
+number[1] = 20      // changing  2 at index 1 to 20
+
+console.log(number) // [10, 20, 3, 4, 5]
+
+const countriess = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+countriess[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
+let lasttIndex = countriess.length - 1
+countriess[lasttIndex] = 'Korea' // Replacing Kenya by Korea
+
+console.log(countriess)
+
+{/* Methods to manipulate array
+There are different methods to manipulate an array. 
+These are some of the available methods to deal with arrays
+=> Array,
+    const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+
+const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
+console.log(four4values) // [4, 4, 4, 4]
+
+=> length,
+    Length:To know the size of the array
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.length) // -> 5 is the size of the array
+
+
+=> concat,
+    concat:To concatenate two arrays.
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+const fruits = ['banana', 'orange', 'mango', 'lemon']                 // array of fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of vegetables
+const fruitsAndVegetables = fruits.concat(vegetables)                 // concatenate the two arrays
+
+console.log(fruitsAndVegetables)
+
+=> indexOf,
+=>slice, 
+=>splice,
+ 
+=> toString,
+
+=> includes, 
+To check if an item exist in an array. If it exist it returns the true else it returns false.
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.includes(5)) // true
+console.log(numbers.includes(0)) // false
+console.log(numbers.includes(1)) // true
+console.log(numbers.includes(6)) // false
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+] // List of web technologies
+
+console.log(webTechs.includes('Node'))  // true
+console.log(webTechs.includes('C'))     // false
+
+=> lastIndexOf,
+lastIndexOf: It gives the position of the last item in the array. If it exist, it returns the index else it returns -1.
+
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+console.log(numbers.lastIndexOf(2)) // 7
+console.log(numbers.lastIndexOf(0)) // -1
+console.log(numbers.lastIndexOf(1)) //  6
+console.log(numbers.lastIndexOf(4)) //  3
+console.log(numbers.lastIndexOf(6)) // -1
+
+=> isArray,
+    Checking array
+Array.isArray:To check if the data type is an array
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(Array.isArray(numbers)) // true
+
+const number = 100
+console.log(Array.isArray(number)) // false
+
+=> toString 
+Converts array to string
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+
+
+=>  Join 
+Joining array elements
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.join()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+] // List of web technologies
+
+console.log(webTechs.join())       // "HTML,CSS,JavaScript,React,Redux,Node,MongoDB"
+console.log(webTechs.join(' # '))  // "HTML # CSS # JavaScript # React # Redux # Node # MongoDB"
+
+slice
+splice
+=> push
+// syntax
+const arr  = ['item1', 'item2','item3']
+arr.push('new item')
+console.log(arr)
+// ['item1', 'item2','item3','new item']
+
+=>pop 
+ Removing item in the end.
+
+ const numbers = [1, 2, 3, 4, 5]
+numbers.pop() // -> remove one item from the end
+console.log(numbers) // -> [1,2,3,4]
+
+=> shift 
+Removing one array element in the beginning of the array.
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.shift() // -> remove one item from the beginning
+console.log(numbers) // -> [2,3,4,5]
+
+=> unshift 
+Adding array element in the beginning of the array.
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.unshift(0) // -> add one item from the beginning
+console.log(numbers) // -> [0,1,2,3,4,5]
+
+=> reverse
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.reverse() // -> reverse array order
+console.log(numbers) // [5, 4, 3, 2, 1]
+
+numbers.reverse()
+console.log(numbers) // [1, 2, 3, 4, 5]
+
+=> Sorting elements in array
+arrange array elements in ascending order. Sort takes a call
+ back function, we will see how we use sort with a call back function 
+ in the coming sections.
+
+ const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+webTechs.sort()
+console.log(webTechs) // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+webTechs.reverse() // after sorting we can reverse it
+console.log(webTechs) // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+=> Array of arrays
+const firstNums = [1, 2, 3]
+const secondNums = [1, 4, 9]
+
+const arrayOfArray =  [[1, 2, 3], [1, 2, 3]]
+console.log(arrayOfArray[0]) // [1, 2, 3]
+
+ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+ const backEnd = ['Node','Express', 'MongoDB']
+ const fullStack = [frontEnd, backEnd]
+ console.log(fullStack)   // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+ console.log(fullStack.length)  // 2
+ console.log(fullStack[0])  // ["HTML", "CSS", "JS", "React", "Redux"]
+ console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
+ */}
